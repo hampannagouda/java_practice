1932b3769a5e65fdb2d085ed1105e027de243a4e
hampannagouda <hampannagouda18@gmail.com>
2026-03-12 10:07:03 +0530

    feat: Add utilities for checking Disarium and Armstrong numbers and converting integers to binary.

---
+++ IntegerToBinary.java
@@ -0,0 +1,22 @@
+package java_practice;
+
+import java.util.Scanner;
+
+public class IntegerToBinary {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter the number :");
+		int num = in.nextInt();
+		System.out.println("the binary of the number is :"+toBinary(num));
+	}
+	static String toBinary(int num) {
+		String s = "";
+		while(num != 0) {
+			int rem = num%2;
+			s = rem+s;
+			num/=2;
+		}
+		return s;
+	}
+}
---
+++ disarium_number.java
@@ -0,0 +1,39 @@
+package java_practice;
+import java.util.*;
+
+public class disarium_number {
+	
+	public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
+			System.out.println("enter the number :");
+			int num = in.nextInt();
+			int count = count(num);
+			int sum = sum(num,count);
+			if(sum == num) {
+				System.out.println("the number is disarium number");
+			}else {
+				System.out.println("the number is not disarium number");
+			}
+		}
+		static int count(int num) {
+			int count = 0;
+			while(num != 0) {
+				count++;
+				num/=10;
+			}
+			return count;
+		}
+		static int sum(int num,int count) {
+			int sum = 0;
+			while(num != 0) {
+				int rem = num %10;
+				sum += Math.pow(rem, count);
+				num /=10;
+				count--;
+			}
+			return sum;
+		}
+	
+
+
+}
